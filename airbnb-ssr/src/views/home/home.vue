4aa361ea173e097f1a62107e01bc61ede36e1fc8
qixin <qixin2@delant.com.cn>
2022-10-24 17:22:10 +0800

    3-5

--- airbnb-ssr/src/views/home/home.vue
+++ airbnb-ssr/src/views/home/home.vue
@@ -2,7 +2,7 @@
  * @Author: qixin qixin2@delant.com.cn
  * @Date: 2022-10-24 16:10:43
  * @LastEditors: qixin qixin2@delant.com.cn
- * @LastEditTime: 2022-10-24 17:00:12
+ * @LastEditTime: 2022-10-24 17:18:34
  * @FilePath: /imooc-nuxt-project/airbnb-ssr/src/views/home/home.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -22,6 +22,8 @@ proxy.$message({
         h('i', { style: 'color: teal' }, 'VNode'),
     ]),
 });
+
+function aa(){}
 
 </script>
 
