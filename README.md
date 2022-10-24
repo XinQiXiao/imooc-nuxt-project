ee16f3e8ed2ec64f139756137285557710c38aa0
qixin <qixin2@delant.com.cn>
2022-10-24 18:42:15 +0800

    3-6

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
  * @Author: qixin qixin2@delant.com.cn
  * @Date: 2022-10-08 18:27:29
  * @LastEditors: qixin qixin2@delant.com.cn
- * @LastEditTime: 2022-10-24 17:12:47
+ * @LastEditTime: 2022-10-24 18:30:53
  * @FilePath: /study/demo/imooc-nuxt-project/README.md
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -62,6 +62,8 @@ plugins: [
     eslint 集成
     npm install eslint -D
     npx eslint --init
+
+    sass 安装
 
 
 [资料]（https://www.imooc.com/wiki/SSR/a2.html）
--- airbnb-ssr/vite.config.ts
+++ airbnb-ssr/vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: qixin qixin2@delant.com.cn
  * @Date: 2022-10-17 13:12:51
  * @LastEditors: qixin qixin2@delant.com.cn
- * @LastEditTime: 2022-10-24 17:01:42
+ * @LastEditTime: 2022-10-24 18:37:51
  * @FilePath: /imooc-nuxt-project/airbnb-ssr/vite.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,6 +11,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,6 +23,19 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     }),
-  ]
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    },
+  },
+  css: {
+    // css 预处理 路径最后要加上；不然会报错
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "@/assets/scss/main.scss";'
+      }
+    }
+  },
 })
 
