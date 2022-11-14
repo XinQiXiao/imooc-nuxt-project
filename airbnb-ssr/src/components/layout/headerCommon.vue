9bb85d3b42f29f9732568b3ddd20b1482db0b8a7
qixin <qixin2@delant.com.cn>
2022-11-14 15:39:02 +0800

    登出失败情况

--- airbnb-ssr/src/components/layout/headerCommon.vue
+++ airbnb-ssr/src/components/layout/headerCommon.vue
@@ -2,7 +2,7 @@
  * @Author: qixin qixin2@delant.com.cn
  * @Date: 2022-11-03 13:36:11
  * @LastEditors: qixin qixin2@delant.com.cn
- * @LastEditTime: 2022-11-10 15:26:40
+ * @LastEditTime: 2022-11-14 15:33:56
  * @FilePath: /imooc-nuxt-project/airbnb-ssr/src/components/layout/headerCommon.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -76,13 +76,17 @@ const userStatus = localStorage.getItem('userStatus')
 // 登出接口
 function userLogout() {
   userLogoutApi().then((res: IResultOr) => {
+    console.log('userLogout res=>', res)
     const { success, message } = res
     if (success) {
       proxy.$message.success(message)
       router.push({ name: 'login' })
       localStorage.setItem('userStatus', '0')
     } else {
+      // 错误情况也允许退出
       proxy.$message.error(message)
+      router.push({ name: 'login' })
+      localStorage.setItem('userStatus', '0')
     }
   })
 }
